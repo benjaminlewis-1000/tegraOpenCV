8ba6ffb23334830b3d3bbd1da095e4836df1ccae
Ben <speedyswimmer1000@gmail.com>
2015-11-11 16:23:21 -0700

    more

--- test.cpp
+++ test.cpp
@@ -1,5 +1,4 @@
 #include <cv.h>
-#include <highgui.h>
 #include <iostream>
 #include "opencv2/opencv.hpp"
 #include "opencv2/gpu/gpu.hpp"
@@ -11,7 +10,20 @@ using namespace std;
 int main(int argc, char** argv){
 	int cuda = cv::gpu::getCudaEnabledDeviceCount();
 	cout << cuda << endl;
+//	cout << "Num processors is " << cv::gpu::DeviceInfo::multiProcessorCount() << endl;
+//	cout << cv::gpu::DeviceInfo::isCompatible() << endl;
+	cv::gpu::DeviceInfo info;
+	cout << info.multiProcessorCount() << endl;
 	cv::gpu::setDevice(0);
 	cv::gpu::GpuMat aa;
 	
+	cv::Mat src_host = cv::imread("img.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	cv::gpu::GpuMat dst, src;
+	src.upload(src_host);
+
+	cv::gpu::threshold(src,dst, 128.0, 255.0, CV_THRESH_BINARY);
+
+	cv::Mat result_host(dst);
+	cv::imshow("Result", result_host);
+	cv::waitKey();
 }

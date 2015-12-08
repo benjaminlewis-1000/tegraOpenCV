0e57805b69c5bac80114e5cf4578a6d6188aa12b
Ben <speedyswimmer1000@gmail.com>
2015-12-08 09:35:40 -0700

    final

--- CMakeFiles/test.dir/DependInfo.cmake
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ SET(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ubuntu/opencv_test/test.cpp" "/home/ubuntu/opencv_test/CMakeFiles/test.dir/test.cpp.o"
+  "/home/benjamin/gitRepos/tegraOpenCV/test.cpp" "/home/benjamin/gitRepos/tegraOpenCV/CMakeFiles/test.dir/test.cpp.o"
   )
 SET(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -14,8 +14,7 @@ SET(CMAKE_TARGET_LINKED_INFO_FILES
 
 # The include file search paths:
 SET(CMAKE_C_TARGET_INCLUDE_PATH
-  "/usr/local/include/opencv"
-  "/usr/local/include"
+  "/usr/include/opencv"
   )
 SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
 SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
